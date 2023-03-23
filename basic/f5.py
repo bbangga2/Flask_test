ad0f2ec578a82ea20d533a908eff73a6154e85a4
bbangga2 <luffy2357@naver.com>
2023-03-23 15:36:45 +0900

    feat: post 데이터 전송 part2

--- basic/f5.py
+++ basic/f5.py
@@ -35,7 +35,7 @@ app = Flask(__name__)
 def login():
     # method 별 분기
     if request.method == 'GET':
-        return "helloworld"
+        return render_template('login.html')
     else: #post
         # request.form['uid'] #값이 누락되면 서버 셧다운이 된다. 사용 금지
         # 1. 로그인 정보 획득 
